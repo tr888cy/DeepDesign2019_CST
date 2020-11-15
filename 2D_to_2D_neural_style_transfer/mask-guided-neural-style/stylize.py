272474c065e68ebbe8b1ac8b0ee9e0853c42de7f
flame666 <924257052@qq.com>
2020-11-15 15:51:29 +0800

    modify output name

--- 2D_to_2D_neural_style_transfer/mask-guided-neural-style/stylize.py
+++ 2D_to_2D_neural_style_transfer/mask-guided-neural-style/stylize.py
@@ -360,7 +360,10 @@ def  main(args):
     print('Iteration %d: loss = %f' % (args.iteration, sess.run(total_loss)))
     result = sess.run(target_net['input'])
     #output_path = os.path.join(args.output_dir, 'result_final.png')
-    output_path = os.path.join(args.output_dir, os.path.split(os.path.splitext(args.content_img)[0])[1]+'2'+os.path.split(os.path.splitext(args.style_img)[0])[1]+ '.png')
+    
+    output_path = os.path.join("StyleWeight " + str(args.style_weight) + "ContentWeight" + str(args.content_weight) + "Iters " + str(args.iteration)
+        ,os.path.join(args.output_dir, os.path.split(os.path.splitext(args.content_img)[0])[1]+'2'+os.path.split(os.path.splitext(args.style_img)[0])[1]+ '.png')) 
+
     #import pdb
     #pdb.set_trace()
     write_image(output_path, result)
